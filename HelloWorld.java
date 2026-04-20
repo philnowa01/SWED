ffc01cacdb1e2efb9983be4d05cf78a6468d790e
philnowa01 <philnowa55@gmail.com>
2026-04-20 17:42:11 +0200

    Add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        int result = 76 + 3;
+        System.out.println(result);
+
+        result = result - 3;
+        System.out.println(result);
+
+        result = result / 3;
+        System.out.println(result);
+
+        result = result + 1337;
+
+        int number = 2;
+        if (number >= 0)
+            if (number == 0)
+                System.out.println("Cow");
+        else System.out.println("Donkey");
+        System.out.println("Cat");
+
+    }
+}
